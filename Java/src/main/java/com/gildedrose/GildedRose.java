c49d35c86f1458e31ed1d833d22f82022edfb513
Dariel Rivero LLerena <dariel@Dariels-MacBook-Pro.local>
2020-08-10 22:44:07 -0400

    Refactor: There are clear if foreach kind of items

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,7 +1,10 @@
 package com.gildedrose;
 
 class GildedRose {
-    Item[] items;
+    private Item[] items;
+    private final String sulfuras = "Sulfuras, Hand of Ragnaros";
+    private final String backstage = "Backstage passes to a TAFKAL80ETC concert";
+    private final String agedBrie = "Aged Brie";
 
     public GildedRose(Item[] items) {
         this.items = items;
@@ -9,53 +12,50 @@ class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
+
+
+            if(items[i].name.equals(agedBrie)) {
                 if (items[i].quality < 50) {
                     items[i].quality = items[i].quality + 1;
-
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
                 }
-            }
-
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
+                if (items[i].sellIn < 0) {
                     if (items[i].quality < 50) {
                         items[i].quality = items[i].quality + 1;
                     }
                 }
+                items[i].sellIn = items[i].sellIn - 1;
+            }
+
+            if(items[i].name.equals(backstage)){
+                if (items[i].quality < 50) {
+                    items[i].quality = items[i].quality + 1;
+                }
+                if (items[i].sellIn < 11) {
+                    if (items[i].quality < 50) {
+                        items[i].quality = items[i].quality + 1;
+                    }
+                }
+                if (items[i].sellIn < 6) {
+                    if (items[i].quality < 50) {
+                        items[i].quality = items[i].quality + 1;
+                    }
+                }
+                if (items[i].sellIn < 0) {
+                    items[i].quality = 0;
+                }
+                items[i].sellIn = items[i].sellIn - 1;
+            }
+
+            if (!items[i].name.equals(agedBrie) && !items[i].name.equals(backstage) && !items[i].name.equals(sulfuras)) {
+                if (items[i].sellIn < 0) {
+                    if (items[i].quality > 0) {
+                        items[i].quality = items[i].quality - 1;
+                    }
+                }
+                if (items[i].quality > 0) {
+                    items[i].quality = items[i].quality - 1;
+                }
+                items[i].sellIn = items[i].sellIn - 1;
             }
         }
     }
