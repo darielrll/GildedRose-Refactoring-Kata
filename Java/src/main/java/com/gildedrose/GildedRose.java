2f4abf057ed1771b9b55e0444374c7eb1638bc99
Dariel Rivero LLerena <dariel@Dariels-MacBook-Pro.local>
2020-08-11 00:11:44 -0400

    Rules were encapsulated inside methods

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -2,61 +2,77 @@ package com.gildedrose;
 
 class GildedRose {
     private Item[] items;
-    private final String sulfuras = "Sulfuras, Hand of Ragnaros";
-    private final String backstage = "Backstage passes to a TAFKAL80ETC concert";
-    private final String agedBrie = "Aged Brie";
 
     public GildedRose(Item[] items) {
         this.items = items;
     }
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-
-
-            if(items[i].name.equals(agedBrie)) {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-                }
-                if (items[i].sellIn < 0) {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
-                items[i].sellIn = items[i].sellIn - 1;
+        for (Item item : items) {
+            if (isLegendaryItem(item)) {
+                continue;
             }
-
-            if(items[i].name.equals(backstage)){
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-                }
-                if (items[i].sellIn < 11) {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
-                if (items[i].sellIn < 6) {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
-                if (items[i].sellIn < 0) {
-                    items[i].quality = 0;
-                }
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-
-            if (!items[i].name.equals(agedBrie) && !items[i].name.equals(backstage) && !items[i].name.equals(sulfuras)) {
-                if (items[i].sellIn < 0) {
-                    if (items[i].quality > 0) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-                if (items[i].quality > 0) {
-                    items[i].quality = items[i].quality - 1;
-                }
-                items[i].sellIn = items[i].sellIn - 1;
-            }
+            notLegendaryApplyUpdateQuality(item);
         }
+    }
+
+    private void notLegendaryApplyUpdateQuality(Item item) {
+        item.sellIn--;
+
+        switch (item.name){
+            case "Aged Brie":
+                brieItemApplyQualityRules(item);
+                break;
+            case "Backstage passes to a TAFKAL80ETC concert":
+                backstageItemApplyQualityRules(item);
+                break;
+            default:
+                unknownItemApplyQualityRules(item);
+        }
+    }
+
+    private void brieItemApplyQualityRules(Item item) {
+        if (item.quality < 49) {
+            item.quality++;
+        }
+        if (item.sellIn < 0) {
+            item.quality++;
+        }
+    }
+
+    private void backstageItemApplyQualityRules(Item item) {
+        if(item.sellIn >= 11){
+            return;
+        }
+        if (item.sellIn < 0) {
+            item.quality = 0;
+            return;
+        }
+        item.quality = increaseQuality(item.quality, 2);
+        if (item.sellIn < 6) {
+            item.quality = increaseQuality(item.quality, 2);
+        }
+    }
+
+    private void unknownItemApplyQualityRules(Item item) {
+        if (item.sellIn < 0) {
+            item.quality = decreaseQuality(item.quality);
+        }
+        item.quality = decreaseQuality(item.quality);
+    }
+
+    private boolean isLegendaryItem(Item item) {
+        return "Sulfuras, Hand of Ragnaros".equals(item.name);
+    }
+
+    private int increaseQuality(int quality, int count) {
+        if(count == 0  ||  quality >= 50){
+            return quality;
+        }
+        return increaseQuality(++quality, --count);
+    }
+
+    private int decreaseQuality(int quality) {
+        return quality > 0 ? quality - 1 : quality;
     }
 }
