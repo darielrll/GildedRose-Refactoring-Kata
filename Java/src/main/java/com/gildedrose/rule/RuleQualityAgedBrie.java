577b05cf4c92b41ec5b1d24b7fc8e388796b57ab
d0r019w <d0r019w@mac-d0r019s.local>
2020-08-13 11:45:30 -0400

    Refactor Aged Brie update quality method to make the code speak

--- Java/src/main/java/com/gildedrose/rule/RuleQualityAgedBrie.java
+++ Java/src/main/java/com/gildedrose/rule/RuleQualityAgedBrie.java
@@ -8,11 +8,22 @@ public class RuleQualityAgedBrie extends RuleQualityNotLegendaryItem{
     public void updateQuality(Item item) {
         super.updateQuality(item);
 
-        if (item.quality < 49) {
-            item.quality++;
+        if (hasExpiredSellDate(item)) {
+            item.quality = increaseQuality(item.quality, 2);
         }
-        if (item.sellIn < 0) {
-            item.quality++;
+        else {
+            item.quality = increaseQuality(item.quality, 1);
         }
+    }
+
+    private boolean hasExpiredSellDate(Item item) {
+        return item.sellIn < 0;
+    }
+
+    private int increaseQuality(int quality, int count) {
+        if(count == 0  ||  quality >= 50){
+            return quality;
+        }
+        return increaseQuality(++quality, --count);
     }
 }
--- Java/src/main/java/com/gildedrose/rule/RuleQualitySulfuras.java
+++ Java/src/main/java/com/gildedrose/rule/RuleQualitySulfuras.java
@@ -5,5 +5,5 @@ import com.gildedrose.Item;
 public class RuleQualitySulfuras extends Rule{
 
     @Override
-    public void updateQuality(Item item) {}
+    public final void updateQuality(Item item) {}
 }
